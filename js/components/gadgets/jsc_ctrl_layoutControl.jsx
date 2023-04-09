aad33e0959af1c271a971976493d70e54d7ec0d2
Mohammad Hefny <mohammad.hefny@gmail.com>
2023-04-09 03:59:22 +0200

    fix: ctrl+r for camera, rename click fixed, experimental feature enable-disable

--- js/components/gadgets/jsc_ctrl_layoutControl.jsx
+++ js/components/gadgets/jsc_ctrl_layoutControl.jsx
@@ -13,7 +13,7 @@ export class CLSS_CTRL_Layout extends React.Component {
             <div id="main_btn_group"  role="group" aria-label="Basic example">
                 <button type="button" id="btn_showSettings" className="btn btn-success btn-sm ctrlbtn" title='Show/Hide Settings Section' onClick={(e) => fn_showSettings()}><strong>SETTINGS</strong></button>
                 <button type="button" id="btn_showMap" className="btn btn-danger btn-sm ctrlbtn" title='Show Map (ctrl+m)' onClick={(e) => fn_showMap()}><strong>MAP</strong></button>
-                <button type="button" id="btn_showVideo" className="btn btn-warning btn-sm ctrlbtn" title='Show Video (ctrl+c)' onClick={(e) => fn_showVideoMainTab()}><strong>CAMERA</strong></button>
+                <button type="button" id="btn_showVideo" className="btn btn-warning btn-sm ctrlbtn" title='Show Video (ctrl+r)' onClick={(e) => fn_showVideoMainTab()}><strong>CAMERA</strong></button>
                 <button type="button" id="btn_showControl" className="btn btn-primary btn-sm ctrlbtn" title='Change Screen Layout' onClick={(e) => fn_showControl()}><strong>{'DISPLAY-' + v_display_mode}</strong></button>
             </div>
         );
