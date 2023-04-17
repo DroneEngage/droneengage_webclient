e7a6354dcff16ca93cdf03023c850eb57e254bb9
Mohammad Hefny <mohammad.hefny@gmail.com>
2023-04-17 19:50:38 +0200

    feature: remove # frrom GCSID in the login.. applied hidden only

--- js/components/jsc_login.jsx
+++ js/components/jsc_login.jsx
@@ -54,7 +54,7 @@ export class CLSS_LoginControl extends React.Component {
 			window.AndruavLibs.LocalStorage.fn_setAccessCode($('#txtAccessCode').val());
 			var s = $('#txtUnitID').val();
 			if (s != null) {
-				window.AndruavLibs.LocalStorage.fn_setUnitID(s.slice(0, s.indexOf('#')));
+				window.AndruavLibs.LocalStorage.fn_setUnitID(s);
 			}
 			window.AndruavLibs.LocalStorage.fn_setGroupName($('#txtGroupName').val());
 
@@ -72,19 +72,6 @@ export class CLSS_LoginControl extends React.Component {
 		fn_connect();
 		// EventEmitter.fn_unsubscribe ('Oppa',this.state.Me);
 	}
-
-	clickCopyURL(e) {
-
-		window.AndruavLibs.LocalStorage.fn_setEmail($('#txtEmail').val());
-		window.AndruavLibs.LocalStorage.fn_setAccessCode($('#txtAccessCode').val());
-		window.AndruavLibs.LocalStorage.fn_setGroupName($('#txtGroupName').val());
-		var un = $('#txtUnitID').val();
-		window.AndruavLibs.LocalStorage.fn_setUnitID(un.slice(0, un.indexOf('#')));
-
-		var connectURL = 'http://' + window.location.hostname + ':' + window.location.port + '/' + window.location.pathname + '?accesscode=' + $('#txtAccessCode').val() + '&email=' + $('#txtEmail').val() + '&groupName=' + $('#txtGroupName').val() + '&unitName=' + $('#txtUnitID').val();
-		fn_copyToClipboard(connectURL);
-	}
-
 
 	componentWillUnmount() {
 		window.AndruavLibs.EventEmitter.fn_unsubscribe(EE_onSocketStatus, this);
@@ -112,7 +99,7 @@ export class CLSS_LoginControl extends React.Component {
 			$('#txtEmail').val(window.AndruavLibs.LocalStorage.fn_getEmail());
 			$('#txtAccessCode').val(window.AndruavLibs.LocalStorage.fn_getAccessCode());
 			$('#txtGroupName').val(window.AndruavLibs.LocalStorage.fn_getGroupName());
-			$('#txtUnitID').val(window.AndruavLibs.LocalStorage.fn_getUnitID() + "#" + fn_generateRandomString(2));
+			$('#txtUnitID').val(window.AndruavLibs.LocalStorage.fn_getUnitID());
 
 		}
 
@@ -146,7 +133,9 @@ export class CLSS_LoginControl extends React.Component {
 							<div className="form-group al_l"><label htmlFor="txtEmail" id="email" className="text-white">Email</label><input type="email" id="txtEmail" name="txtEmail" className="form-control" defaultValue={QueryString.email != null ? QueryString.email : window.AndruavLibs.LocalStorage.fn_getEmail()} /></div>
 							<div className="form-group al_l"><label htmlFor="txtAccessCode" id="account" className="text-white">Access Code</label><input type="password" id="txtAccessCode" name="txtAccessCode" className="form-control" defaultValue={QueryString.accesscode != null ? QueryString.accesscode : window.AndruavLibs.LocalStorage.fn_getAccessCode()} /></div>
 							<div className="form-group al_l hidden"><label id="group" className="text-white">Group Name</label> <input type="text" id="txtGroupName" name="txtGroupName" className="form-control" defaultValue={QueryString.groupName != null ? QueryString.groupName : window.AndruavLibs.LocalStorage.fn_getGroupName()} /></div>
-							<div className="form-group al_l"><label id="unitID" className="text-muted">Unit ID</label><input type="text" id="txtUnitID" name="txtUnitID" className="form-control" defaultValue={QueryString.unitName != null ? QueryString.unitName : window.AndruavLibs.LocalStorage.fn_getUnitID() + "_" + fn_generateRandomString(2)} /></div>
+							<div className="form-group al_l"><label id="unitID" className="text-muted">GCS ID</label>
+								<input type="text" id="txtUnitID" name="txtUnitID" className="form-control" defaultValue={QueryString.unitName != null ? QueryString.unitName : window.AndruavLibs.LocalStorage.fn_getUnitID()} />
+								<input type="hidden" id="txtUnitID_ext" name="txtUnitID_ext" value={"_" + fn_generateRandomString(2)}/></div>
 							<br />
 						</div>
 						<div id='login_btn mb-2 ' className='text-center'>
@@ -176,7 +165,9 @@ export class CLSS_LoginControl extends React.Component {
 								<div className="form-group al_l"><label htmlFor="txtEmail" id="email" className="text-white">Email</label><input type="email" id="txtEmail" name="txtEmail" className="form-control" defaultValue={QueryString.email != null ? QueryString.email : window.AndruavLibs.LocalStorage.fn_getEmail()} /></div>
 								<div className="form-group al_l"><label htmlFor="txtAccessCode" id="account" className="text-white" title="Access Code" >Password</label><input type="password" id="txtAccessCode" title="Access Code" name="txtAccessCode" className="form-control" defaultValue={QueryString.accesscode != null ? QueryString.accesscode : window.AndruavLibs.LocalStorage.fn_getAccessCode()} /></div>
 								<div className="form-group al_l hidden"><label id="group" className="text-white">Group Name</label> <input type="text" id="txtGroupName" name="txtGroupName" className="form-control" defaultValue={QueryString.groupName != null ? QueryString.groupName : window.AndruavLibs.LocalStorage.fn_getGroupName()} /></div>
-								<div className="form-group al_l"><label id="unitID" className="text-muted">GCS ID</label><input type="text" id="txtUnitID" name="txtUnitID" className="form-control" defaultValue={QueryString.unitName != null ? QueryString.unitName : window.AndruavLibs.LocalStorage.fn_getUnitID() + "_" + fn_generateRandomString(2)} /></div>
+								<div className="form-group al_l"><label id="unitID" className="text-muted">GCS ID</label>
+								<input type="text" id="txtUnitID" name="txtUnitID" className="form-control" defaultValue={QueryString.unitName != null ? QueryString.unitName : window.AndruavLibs.LocalStorage.fn_getUnitID()} />
+								<input type="hidden" id="txtUnitID_ext" name="txtUnitID_ext" value={"_" + fn_generateRandomString(2)}/></div>
 								<br />
 							</div>
 							<div id='login_btn mb-2 ' className='text-center'>
