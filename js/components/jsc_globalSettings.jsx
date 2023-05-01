f5555d525598b689e5fd637bdd6f60e402d5584c
Mohammad Hefny <mohammad.hefny@gmail.com>
2023-05-01 10:30:28 +0300

    fix: remove unused code

--- js/components/jsc_globalSettings.jsx
+++ js/components/jsc_globalSettings.jsx
@@ -150,8 +150,6 @@ class CLSS_Preferences extends React.Component {
       $('#check_enable_speech')[0].checked = window.AndruavLibs.LocalStorage.fn_getSpeechEnabled();
       $('#volume_range')[0].value = window.AndruavLibs.LocalStorage.fn_getVolume();
       $('#check_tabs_display')[0].checked = window.AndruavLibs.LocalStorage.fn_getTabsDisplayEnabled();
-      //$('#chk_google_key')[0].checked = window.AndruavLibs.LocalStorage.fn_getGoogleMapEnabled();
-      //$('#txt_google_key').val(window.AndruavLibs.LocalStorage.fn_getGoogleMapKey());
   }
 
 
@@ -205,41 +203,19 @@ class CLSS_Preferences extends React.Component {
     window.AndruavLibs.EventEmitter.fn_dispatch (EE_onPreferenceChanged);
   }
 
-  fn_enableGoogleMaps()
-  {
-    const enabled = $('#chk_google_key')[0].checked;
-    window.AndruavLibs.LocalStorage.fn_setGoogleMapEnabled(enabled);
-    if (enabled === true)
-    {
-      $('#txt_google_key').show();
-    }
-    else
-    {
-      $('#txt_google_key').hide();
-    }
-  }
-
   fn_keydown()
   {
     window.AndruavLibs.LocalStorage.fn_setGoogleMapKey($('#txt_google_key').val());
   }
 
   render () {
-    var google_preference=[];
     var v_speech_disabled = 'false';
     if (window.AndruavLibs.LocalStorage.fn_getSpeechEnabled()===false)
     {
       v_speech_disabled = 'true';
     }
 
-    google_preference.push(<div className="row mb-12">
-                            <label className="col-sm-4 col-form-label al_l " >Google Maps</label>
-                            <input className="form-check-input col-sm-2 " type="checkbox" id="chk_google_key" onClick={ () => this.fn_enableGoogleMaps()} />
-                            <div className="col-sm-4 col-form-label al_l " >
-                            <input id='txt_google_key' type="text" className="form-control" aria-describedby="emailHelp" placeholder="enter valid API Key" onPaste={() => this.fn_keydown()} onKeyDown={() => this.fn_keydown()}/>
-                            </div>
-                          </div>);
-      return (
+    return (
           <fieldset>
             <div className="row mb-12 align-items-center">
               <label className="col-sm-4 col-form-label al_l" >Enable Speech</label>
