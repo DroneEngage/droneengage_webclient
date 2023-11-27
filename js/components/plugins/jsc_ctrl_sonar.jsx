83f62ae391a3c1a94d4d7147831e89ca61c61951
Mohammad Hefny <mohammad.hefny@gmail.com>
2023-11-27 21:25:20 +0200

    fix: react keys

--- js/components/plugins/jsc_ctrl_sonar.jsx
+++ js/components/plugins/jsc_ctrl_sonar.jsx
@@ -75,21 +75,20 @@ export class CLSS_CTRL_SONAR    extends React.Component {
         
         if ((c_sonar.m_is_initialized===true) && (c_sonar.m_is_paused===false))
         {
-            v_sonar_buttom.push(<p key={Math.random().toString(36)}  className="text-success">Sonar is Active <span className="text-success">{c_sonar.m_log_filename}</span></p>);
+            v_sonar_buttom.push(<p key={v_andruavUnit.partyID + 'sonar_ctrl_sb1'}  className="text-success">Sonar is Active <span className="text-success">{c_sonar.m_log_filename}</span></p>);
         }
         else
         {
-            v_sonar_buttom.push(<p key={Math.random().toString(36)}  className="text-danger">Sonar is Inactive <span className="text-light">{c_sonar.m_log_filename}</span></p>);
+            v_sonar_buttom.push(<p key={v_andruavUnit.partyID + 'sonar_ctrl_sb2'} className="text-danger">Sonar is Inactive <span className="text-light">{c_sonar.m_log_filename}</span></p>);
         }
 
-
-
+        
         return(
-            <div key={Math.random().toString(36)} id='sonar_1' className= 'row al_l  css_margin_zero '>
-                <div key={Math.random().toString(36)} className = 'row al_l css_margin_zero d-flex '>
-                    <div key={Math.random().toString(36)}  className= 'col-12 user-select-none '>
+            <div key={v_andruavUnit.partyID + 'sonar_ctrl'} id='sonar_1' className= 'row al_l  css_margin_zero '>
+                <div key={v_andruavUnit.partyID + 'sonar_ctrl_1'} className = 'row al_l css_margin_zero d-flex '>
+                    <div key={v_andruavUnit.partyID + 'sonar_ctrl_11'}  className= 'col-12 user-select-none '>
                         {v_sonar_buttom}
-                        <div key={Math.random().toString(36)} id="main_btn_group"  role="group" aria-label="Basic example">
+                        <div key={v_andruavUnit.partyID + 'sonar_ctrl_111'} id="main_btn_group"  role="group" aria-label="Basic example">
                         <button type="button" id="btn_startSonar" className="btn btn-success btn-sm ctrlbtn" title='Start Sonar' onClick={(e) => this.fn_resumeSonar(v_andruavUnit)}><strong>Start Sonar</strong></button>
                         <button type="button" id="btn_stopSonar" className="btn btn-danger btn-sm ctrlbtn" title='Stop SOnar' onClick={(e) => this.fn_pauseSonar(v_andruavUnit)}><strong>Pause Sonar</strong></button>
                         <button type="button" id="btn_refreshSonar" className="btn btn-warning btn-sm ctrlbtn" title='Stop SOnar' onClick={(e) => this.fn_getSonarStatus(v_andruavUnit)}><strong>Refresh</strong></button>
