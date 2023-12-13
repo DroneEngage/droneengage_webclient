e40b3a17ed81cdd7ee26fd3e160a6d37922c23c8
Mohammad Hefny <mohammad.hefny@gmail.com>
2023-12-14 01:23:05 +0200

    feature: adding build number

--- js/components/jsc_footer.jsx
+++ js/components/jsc_footer.jsx
@@ -13,7 +13,7 @@ class CLSS_FooterControl extends React.Component {
         return (
             <footer className="text-center bg-4">
   
-                <p className="user-select-none  text-white">© Copyright  2014-{year}, <a href={CONST_HOME_URL} className="a_nounderline a_hoverinvers"data-toggle="tooltip" title="DroneEngage">{CONST_TITLE}</a></p> 
+                <p className="user-select-none  text-white">© Copyright  2014-{year}, <a href={CONST_HOME_URL} className="a_nounderline a_hoverinvers"data-toggle="tooltip" title="DroneEngage">{CONST_TITLE}</a> <span className="small text-light text-decoration-underline">  build:{build_number}</span></p> 
     
             </footer>
         );
